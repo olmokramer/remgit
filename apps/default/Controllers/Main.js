3c5b077344d4ee9203c1b63f9bb333ad7fc1695c
Wouter Vroege <wouter@woutervroege.nl>
2012-12-10 12:24:46 +0100

    apprise plugin (confirm delete)
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

--- apps/default/Controllers/Main.js
+++ apps/default/Controllers/Main.js
@@ -164,10 +164,10 @@ var Main = {
 
 			switch(Main.currentTypeOfMain3Data){
 			case 'document':
-				Document.drop();
+				Main.confirmDelete("document");
 				break;
 			case 'mediaItem':
-				Media.drop();
+				Main.confirmDelete("media");
 				break;
 			}
 
@@ -194,7 +194,24 @@ var Main = {
 			}
 		});
 		$('.logout').click(function() {Logger.logout()});
+	},
+
+	confirmDelete: function (action, id) {
+		apprise('Are You Sure you want to delete this item?', {'verify':true}, function(r) {
+		if(r) { 
+			switch(action) {
+			case 'document':
+				Document.drop();
+				break;
+			case 'media':
+				Media.drop();
+				break;
+			}
+		}
+		else { }
+	    })
 	}
+
 }
 
 $(document).ready(function() {
