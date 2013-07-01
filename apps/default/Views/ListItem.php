7332d13d4dcf0cf39373e2db1c25fdff0d39f0d2
Olmo Kramer <olmo.kramer@gmail.com>
2013-07-02 00:01:40 +0200

    minor fixes

--- apps/default/Views/ListItem.php
+++ apps/default/Views/ListItem.php
@@ -11,31 +11,31 @@ class ListItem {
 		$this->label .= (strlen($label) > 40) ? '...' : '';
 		$this->secondary = $secondary;
 		$this->mediakind = $mediakind;
-		
+
 		if(isset($imgurl) && strlen($imgurl)>0):
 		$li_style = "background-image:url('".$this->imgurl."');background-size: 20px;background-repeat:no-repeat;";
 		else:
 		$li_style = null;
 		endif;
-		
+
 		?>
-		
+
 		<!-- list item -->
-		
+
 		<li data-type="<?=$this->type?>" data-id="<?=$this->id?>" <?= ($this->mediakind!=null) ? 'data-mediakind="'.$this->mediakind.'"' : ''; ?>>
-			
+
 			<!-- label -->
 			<h6 style="<?=$li_style?>" class="icon-<?=$this->type?>"><?=$this->label?></h6>
-			
+
 			<!-- pubstate -->
 			<?php if($this->secondary != null): ?>
 				<div class="pubstate state-<?=$secondary?>">
 				</div>
 			<?php endif; ?>
-		
+
 		</li>
 		<!-- end list item -->
-		
+
 		<?php
 	}
 }
