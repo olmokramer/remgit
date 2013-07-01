d62b5db3c639635d0038252a4d48bb3404ef5e86
Olmo Kramer <olmo.kramer@gmail.com>
2013-07-01 22:56:48 +0200

    small interface fix in document view

--- apps/default/settings.php
+++ apps/default/settings.php
@@ -1,6 +1,6 @@
 <?php
 //application settings
-define('APP_ROOT', /*APPS_ROOT.'default'/*/'./');
+define('APP_ROOT', APPS_ROOT.'default/');
 define('CONTROLLERS_ROOT', APP_ROOT.'Controllers/');
 define('VIEWS_ROOT', APP_ROOT.'Views/');
 ?>
