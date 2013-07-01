b726a9b9c8b7ed1d82103bcfe1a90f2e623d6341
Olmo Kramer <olmo.kramer@gmail.com>
2013-07-01 22:36:50 +0200

    we moeten echt iets vinden op die config...

--- config/DB.php
+++ config/DB.php
@@ -10,12 +10,12 @@ private static $instance = null;
 	public static function getInstance() {
 		if(self::$instance == null) {
 			try {
-				define("DBHOST", "mysql3.greenhost.nl");
-				define("DBNAME", "luukkramer_nl_rearend2");
-				define("DBUSER", "luukkramer2");
-				define("DBPASS", "MorirWov9");
+				define("DBHOST", "localhost:3306");
+				define("DBNAME", "rearend_manager");
+				define("DBUSER", "root");
+				define("DBPASS", "");
 				self::$instance = new \PDO('mysql:host='.DBHOST.';dbname='.DBNAME.';charset=UTF-8', DBUSER, DBPASS);
-			} 
+			}
 			catch(PDOException $e) {
 				// Handle this properly
 				throw $e;
